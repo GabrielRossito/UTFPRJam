df9d998a18e9584321f0d085f87f21813d4c2093
Unknown <keny_hks@hotmail.com>
2016-10-16 10:55:13 -0200

    Barra de poder e Icones de pider

---
+++ UTFPRJAM/Assets/Scripts/PowerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//namespace da UI da Uinty
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class PowerController : MonoBehaviour
+{
+	public Button buttonCura;
+	public Button buttonEscudo;
+	public Button buttonForca;
+	public Button buttonCarreta;
+
+	public void Cura()
+	{
+		Debug.Log("Curar");
+	}
+
+	public void Escudo()
+	{
+		Debug.Log("Dar Resistência");
+	}
+
+	public void Forca()
+	{
+		Debug.Log("Dar Força");
+	}
+
+	public void Carreta()
+	{
+		Debug.Log("Invocar a Carreta Furacão");
+	}
+}
