f0f0f7efa52e03f6474a8af2f2beed1b1c56ba3d
Unknown <keny_hks@hotmail.com>
2016-10-16 07:48:50 -0200

    Cenas de Menu (Hector)

---
+++ UTFPRJAM/Assets/Scripts/MenuController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//namespace da UI da Uinty
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MenuController : MonoBehaviour
+{
+    public Button buttonPlay;
+    public Button buttonAbout;
+    public Button buttonQuit;
+
+    public void LoadPlayScene()
+    {
+		Debug.Log("Load Play");
+		Application.LoadLevel("sceneIntro");
+    }
+
+    public void LoadAboutScene()
+    {
+		Debug.Log("Load About");
+		Application.LoadLevel("sceneAbout");
+    }
+
+    public void QuitGame()
+    {
+		Debug.Log("Quit Game");
+		Application.Quit();
+    }
+}
---
+++ UTFPRJAM/Assets/Scripts/MenuControllerBack.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//namespace da UI da Uinty
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MenuControllerBack : MonoBehaviour
+{
+	public Button buttonBack;
+
+	public void LoadMenuScene()
+	{
+		Debug.Log("Load Menu");
+		Application.LoadLevel("sceneMenu");
+	}
+}
